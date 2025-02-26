daf62566dfed25d291821054ab234ca6eb3e4e8e
hibasulaiman <hibasulaiman9@gmail.com>
2025-02-26 13:26:03 +0530

    update

--- express/models/Contactmodel.js
+++ express/models/Contactmodel.js
@@ -3,16 +3,14 @@ const mongoose = require('mongoose')
 const contactModel = new mongoose.Schema({
     name: {
         type: String,
-        unique: true,
         required: true
 
     },
     phone: {
-        type: String,
-        unique: true,
+        type: Number,
         required: true
     }
 })
 
-const contacts = mongoose.model("contacts", contactModel)
+const contacts = mongoose.model("Contacts", contactModel)
 module.exports = contacts
--- express/routes/contactroutes.js
+++ express/routes/contactroutes.js
@@ -1,3 +1,21 @@
 const express= require ('express')
 const router = express.router()
-const contacts= require ('../ models/')
+const contacts= require('../models/ContactModel')
+
+
+router.get('/all',async (requestAnimationFrame, res)) => {
+    try {
+        const contacts = await Contacts.find()
+        return res.status(200).json(contacts)
+    } catch (error) {
+        return res.status(500).json({ message: error.message })
+    }
+})
+
+
+router.post('/add', async (req, res) => {
+    try{
+        const newcontact = new Contacts(req.body)
+        const {name, phone }
+    }
+})
